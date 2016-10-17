27c62ac18252ead29289677b0844143ee9d3db38
John Geddes <jgeddes@thoughtworks.com>
2016-10-17 12:44:11 +1000

    [JG] Creating projects and setting up testing harness

---
+++ TDD.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace TDD.Tests.Unit
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            true.ShouldBe(true);
+        }
+    }
+}
